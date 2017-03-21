544e6f47ce648b20dd2ad4e53048d5d22514b11f
Michael Lee <mleenavy@gmail.com>
2017-03-21 15:06:57 -0500

    I wrote some code!!!

--- budget/app/controllers/dashboard_controller.rb
+++ budget/app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @category1 = { category: "Food", amount: rand(1..100) }
-    @category2 = { category: "Rent", amount: rand(1..100) }
-    @category3 = { category: "Savings", amount: rand(1..100) }
-    @category4 = { category: "Transportation", amount: rand(1..100) }
-    @category5 = { category: "Entertainment", amount: rand(1..100) }
-    @category_others = { category: "Bills", amount: rand(1..100) }
+    @category1 = { category: "Food", amount_spent: rand(1..100), under_budget: 0, over_budget: rand(1..30) }
+    @category2 = { category: "Rent", amount_spent: rand(1..100), under_budget: 0, over_budget: 0 }
+    @category3 = { category: "Savings", amount_spent: rand(1..100), under_budget: rand(1..30), over_budget: 0 }
+    @category4 = { category: "Transportation", amount_spent: rand(1..100), under_budget: 0, over_budget: rand(1..30) }
+    @category5 = { category: "Entertainment", amount_spent: rand(1..100), under_budget: 0, over_budget: rand(1..30) }
+    @category_others = { category: "Bills", amount_spent: rand(1..100), under_budget: rand(1..30), over_budget: 0 }
   end
 end
