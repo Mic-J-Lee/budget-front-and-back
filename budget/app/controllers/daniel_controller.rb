09eb14440cbdaa59fc6153db57e3dd43aab73343
Daniel <dkweonlee@gmail.com>
2017-03-22 10:04:07 -0500

    to pull

--- budget/app/controllers/daniel_controller.rb
+++ budget/app/controllers/daniel_controller.rb
@@ -1,4 +1,10 @@
 class DanielController < ApplicationController
   def index
   end
+
+  def login
+  end
+
+  def register
+  end
 end
--- budget/config/routes.rb
+++ budget/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'daniel/index'
-=======
+  get 'daniel/login'
+  get 'daniel/register'
+  
   get 'budget_tables/new'
 
   get 'items/new'
@@ -9,9 +10,7 @@ Rails.application.routes.draw do
   get 'categories/new'
 
   get 'users/new'
->>>>>>> 7ec0cfc7edeaa23e1fa90e3a5034d56a2fbb8c8b
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dashboard
-  resources :login
 end
