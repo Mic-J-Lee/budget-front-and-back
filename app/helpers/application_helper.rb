647d702ac2ad8cd3bb5e22dfe9157bc3d0f81164
Michael Lee <mleenavy@gmail.com>
2017-05-09 17:05:09 -0500

    fixed signup bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
 
   def stringify_month(month)
-    DateTime.new(month.to_s[0..3].to_i,month.to_s[4..5].to_i).strftime("%B, %Y ")
+    if month
+      DateTime.new(month.to_s[0..3].to_i,month.to_s[4..5].to_i).strftime("%B, %Y ")
+    end
   end
 
   
